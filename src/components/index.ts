a539e0b350c81af640f9eb55bc4758635b241602
Алексей Болдырев <84848493+DoneAlejandro@users.noreply.github.com>
2024-06-17 23:27:10 +0300

    updates

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { Header } from './header';
 export { Main } from './main';
 export { Footer } from './footer';
 export { Button } from './button';
-export { Dialog } from './dialog';
 export { Input } from './input';
 export { InputField } from './input-field';
 export { Link } from './link';
